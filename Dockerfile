eb52601a9e6ec1609e001230bb50a1fcc8189ae8
ChinCUstu <ndulue.chinedu@stu.cu.edu.ng>
2023-05-16 16:19:28 +0100

    feat: Add Bible verse memorization code with memorable word generation
    This commit adds a C# code implementation for Bible verse memorization. The code allows users to input their desired Bible verse and generates a list of memorable words to aid in memorization. The memorable words are selected based on emotional significance, distinctiveness, visual imagery, and repetition.
    
    The code includes improvements such as proper variable naming conventions, exception handling, and adherence to best practices. It provides a foundation for further customization and enhancement.
    
    This commit resolves #<issue_number> (if applicable).

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:7.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
+WORKDIR /src
+COPY ["Bible_Verse_Memorization.csproj", "./"]
+RUN dotnet restore "Bible_Verse_Memorization.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "Bible_Verse_Memorization.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "Bible_Verse_Memorization.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "Bible_Verse_Memorization.dll"]
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Bible_Verse_Memorization;
+
+class BibleVerseMemorization
+{
+    // Set of common words to ignore
+    private static readonly string[] IgnoreWords = { "the", "and", "that", "he", "his", "in", "but", "have", "is" };
+
+    private static void Main()
+    {
+        Console.WriteLine("Enter the Bible verse you want to memorize: ");
+        var bibleVerse = Console.ReadLine();
+
+        var memorableWords = GetMemorableWordsFromVerse(bibleVerse);
+
+        Console.WriteLine("\nMemorable Words from the Bible Verse:");
+        foreach (var word in memorableWords)
+        {
+            Console.WriteLine(word);
+        }
+    }
+
+    private static List<string> GetMemorableWordsFromVerse(string? verse)
+    {
+        // Split the verse into words
+        var words = verse?.Split(new[] { ' ', ',', '.', ':', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Filter out ignored words
+        words = (words ?? throw new InvalidOperationException()).Where(word => !IgnoreWords.Contains(word.ToLower()))
+            .ToArray();
+
+        var memorableWords = words
+            .Where(word => HasEmotionalSignificance(word) || IsDistinctive(word) || HasVisualImagery(word)).ToList();
+
+        // Add some words based on repetition to aid memorization
+        memorableWords.AddRange(words.Take(3));
+
+        return memorableWords;
+    }
+
+    private static bool HasEmotionalSignificance(string word)
+    {
+        // Example: Check if the word contains emotionally significant terms
+        string[] emotionallySignificantTerms = { "love", "eternal life", "forgiveness" };
+
+        return emotionallySignificantTerms.Any(term => word.ToLower().Contains(term));
+    }
+
+    private static bool IsDistinctive(string word)
+    {
+        // Example: Check if the word has a distinct sound
+        string[] distinctiveSounds = { "so", "world", "perish" };
+
+        return distinctiveSounds.Contains(word.ToLower());
+    }
+
+    private static bool HasVisualImagery(string word)
+    {
+        // Example: Check if the word triggers visual imagery
+        string[] wordsWithVisualImagery = { "Son", "believes" };
+
+        return wordsWithVisualImagery.Contains(word);
+    }
+}
